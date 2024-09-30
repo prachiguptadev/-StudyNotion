b0f25cf6c7151d3e98bf6d10270711bd3f05e756
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2024-09-30 22:24:08 +0530

    LearningLanguageSection done

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import HighlightText from "../HomePage/HighlightText";
+import know_your_progress from "../../../assets/Images/Know_your_progress.png";
+import compare_with_others from "../../../assets/Images/Compare_with_others.png";
+import plan_your_lessons from "../../../assets/Images/Plan_your_lessons.png";
+import CTAButton from "../../../components/core/HomePage/Button"
 
 const LearningLanguageSection = () => {
   return (
-    <div>LearningLanguageSection</div>
-  )
-}
+    <div className="mt-[130px]">
+      <div className="flex flex-col gap-5 items-center">
+        <div className="text-4xl font-semibold text-center">
+          Your Swiss Knife for
+          <HighlightText text={"learning any language"} />
+        </div>
 
-export default LearningLanguageSection
+        <div className="text-center text-richblack-600 mx-auto text-base font-medium w-[70%]">
+          Using spin making learning multiple languages easy. with 20+ languages
+          realistic voice-over, progress tracking, custom schedule and more.
+        </div>
+
+        <div className="flex flex-row items-center justify-center mt-5">
+          <img
+            src={know_your_progress}
+            alt="know_your_progress"
+            className="object-contain -mr-32"
+          />
+          <img
+            src={compare_with_others}
+            alt="compare_with_others"
+            className="object-contain"
+          />
+          <img
+            src={plan_your_lessons}
+            alt="plan_your_lessons"
+            className="object-contain -ml-36"
+          />
+        </div>
+
+          <div className="w-fit ">
+            <CTAButton active={true} linkto={"signup"}>
+              {"Learn more"}
+              </CTAButton>
+          </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default LearningLanguageSection;
